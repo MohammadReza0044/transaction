d50ed933b183adb467407ba7aa6a2bec82d60dee
MohammadReza0044 <you@example.com>
2022-05-31 21:17:34 +0430

    authentication is done. (seperUser and normal user creataion)

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+from email.policy import default
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class customUserManager(BaseUserManager):
+
+    def user_create (self, email, password, **extra_fields):
+        if not email:
+            raise ValueError (_("you must enter an email"))
+        email=self.normalize_email(email)
+        new_user = self.model (email=email, **extra_fields)
+        new_user.set_password(password)
+        new_user.save()
+        return new_user
+
+
+    def create_seperUser(self,email,password, **extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+
+        if extra_fields.setdefault('is_staff') is not True:
+            raise ValueError (_('supperuser must have is_staff as True'))
+        
+        if extra_fields.setdefault('is_superuser') is not True:
+            raise ValueError (_('supperuser must have is_superuser as True'))
+
+        if extra_fields.setdefault('is_active') is not True:
+            raise ValueError (_('supperuser must have is_active as True'))
+
+        return self.create_user(email,password,**extra_fields)
+
+
+
+class user (AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return f"<user {self.email}"
+
+
+
+
